c4239bcfba07d1eb5d382ff6c8d814ddc22ac5f6
Mariana Charneca <mb.charneca@gmail.com>
2025-12-11 15:52:51 +0000

    function

---
+++ macros/functions.sql
@@ -0,0 +1,3 @@
+ {% macro margin_percent(revenue, purchase_cost, macro_var=2) %}
+     {{ (SAFE_DIVIDE(revenue - purchase_cost) / revenue * 100) | round(macro_var) }}%
+ {% endmacro %}
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@
       quantity,
       purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin,
+     {{ margin_percent( {{ 'revenue' }}, {{ 'purchase_cost' }}) }} AS margin_percent
   FROM {{ref("stg_raw__raw_gz_sales")}} s
   LEFT JOIN {{ref("stg_raw__raw_gz_product")}} p
       USING (products_id)
